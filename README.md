fae956726c0ffdf711f107882418829aaaf898d7
Daniel Buchner <danieljb2@gmail.com>
2023-03-02 14:32:51 -0600

    push new attempt

--- README.md
+++
@@ -1,2 +0,0 @@
-# PWA Template Project
-A simple template project for PWAs with a service worker and caching.
---
+++ service-worker.js
@@ -0,0 +1,40 @@
+var CACHE_NAME = 'PWA Template';
+var urlsToCache = [
+  'https://henryegloff.com/demos/pwa-template/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
